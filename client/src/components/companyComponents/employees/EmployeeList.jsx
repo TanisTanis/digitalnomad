dc2b11f0da72f047698bb663f878393c6e6ea0f5
Tanis Kiel <tkiel12@gmail.com>
2021-03-12 12:22:22 -0800

    Pagination on employee list

--- client/src/components/companyComponents/employees/EmployeeList.jsx
+++ client/src/components/companyComponents/employees/EmployeeList.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Employee from './Employee';
+import ReactPaginate from 'react-paginate';
+
+const perPage = 5;
 
 const EmployeeList = (props) => {
 
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const offset = currentPage * perPage;
+
+  const currentPageData = props.employees.slice(offset, offset + perPage).map((employee, index) => (
+    <Employee employee={employee} key={employee.lastName + index}/>
+    ));
+
+  const pageCount = Math.ceil(props.employees.length / perPage);
+
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
+
   return(
     <div className="employee-list-container">
-      {props.employees.map((employee, index) => (
-         <Employee employee={employee} key={employee.lastName + index}/>
-      ))}
+      {currentPageData}
+      <ReactPaginate
+        previousLabel={"← Previous"}
+        nextLabel={"Next →"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      />
     </div>
   )
 }
